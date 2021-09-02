13ffee5a10d5c43f0dab9a13799051c68f11eb44
Ankit Sah <ankitkumarsah3727@gmail.com>
2021-09-02 23:39:55 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php include_once("header.php"); ?>
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+            <h4>Best Offer</h4>
+            <h2>New Arrivals On Sale</h2>
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <h4>Flash Deals</h4>
+            <h2>Get your best products</h2>
+          </div>
+        </div>
+    
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+
+    <div class="best-features about-features">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="section-heading">
+                <h2>Our Background</h2>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="right-image">
+                <img src="assets/images/feature-image.jpg" alt="">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="left-content">
+                <h4>Who we are &amp; What we do?</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
+                <ul class="social-icons">
+                  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          
+                </ul>
+                <!-- <a href="about.html" class="filled-button">Read More</a> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="latest-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Gallery</h2>
+            
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+           
+            </div>
+          </div>
+
+      
+        </div>
+      </div>
+    </div>
+
+
+
+
+ 
+
+    <?php include_once("footer.php"); ?>
